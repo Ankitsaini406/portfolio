8f583440eeec0b2a4b3baf65df39eaac213fc123
Ankit Saini <as.ankitsaini406@gmail.com>
2024-06-10 07:45:35 +0530

    "changes"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${style.backgroungColor}`}>
+      <body>
         <Introduction>
           <ThemeProviders>
               <Navbar />
