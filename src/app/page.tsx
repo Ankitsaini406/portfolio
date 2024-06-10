8a0f92f363022439544bd9189bc1bcc0c7f7fd5d
Ankit Saini <as.ankitsaini406@gmail.com>
2024-06-10 10:40:30 +0530

    "home page changes"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.bodyFlex}>
-       <div>
-       <h1>Hi Ankit is here</h1>
+      <div>
+        <h1>Hi Ankit is here</h1>
         <h1>Web Devloper</h1>
         <h1>App Devloper</h1>
-       </div>
-       <div className={styles.heroImage}>
-       </div>
+      </div>
+      <div className={styles.heroImage}></div>
     </main>
   );
 }
