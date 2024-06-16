9a982e66ae646cf1f658552185e3ffcebf908825
Ankit Saini <as.ankitsaini406@gmail.com>
2024-06-16 18:18:24 +0530

    "Hero page"

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -1,14 +1,27 @@
 import styles from "./hero.module.css";
+import { FaHtml5, FaCss3Alt, FaReact, FaShopify, FaPhp } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { RiFlutterFill, RiNextjsFill } from "react-icons/ri";
+import { SiMysql } from "react-icons/si";
+import { TbApi } from "react-icons/tb";
+import { DiResponsive } from "react-icons/di";
+import { blue } from "@mui/material/colors";
+
 
 const Hero = () => {
     return (
         <div className={styles.bodyFlex}>
-            <div className={styles.introtext}>
-                <h1>I&apos;m</h1>
-                <ul className={styles.alltext}>
-                    <li><span className={styles.typingText}>Web Devloper</span></li>
-                    <li><span className={styles.typingText}>App Devloper</span></li>
-                </ul>
+            <div className={styles.herotext}>
+                <div className={styles.introtext}>
+                    <h1>I&apos;m</h1>
+                    <ul className={styles.alltext}>
+                        <li><span className={styles.typingText}>Web Devloper</span></li>
+                        <li><span className={styles.typingText}>App Devloper</span></li>
+                    </ul>
+                </div>
+                <div className={styles.skills}>
+                    <FaHtml5 /> <FaCss3Alt style={{ color: 'blue '}}/> <IoLogoJavascript style={{ color: 'yellow ', backgroundColor: 'black'}}/> <RiFlutterFill  style={{ color: 'skyblue'}}/> <FaReact  style={{ color: 'skyblue'}}/> <RiNextjsFill style={{ color: 'black'}}/> <SiMysql /> <TbApi /> <FaShopify style={{ color: 'green'}}/> <FaPhp />
+                </div>
             </div>
             <div className={styles.heroImage}></div>
         </div>
