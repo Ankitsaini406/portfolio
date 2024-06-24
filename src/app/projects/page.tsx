705c9dbf3f487d9f310174b85cebfec029d780f1
Ankit Saini <as.ankitsaini406@gmail.com>
2024-06-24 17:11:07 +0530

    "project details"

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,53 +1,27 @@
 import Image from 'next/image';
 import style from './projects.module.css'
+import ProjectsDetails from '@/lib/project';
 
 const Projects = () => {
     return (
         <div className={style.project}>
-            <div className={style.projectbox}>
-                <div className={style.imagecontainor}>
+            {
+                ProjectsDetails.map((project) => {
+                    return (
+                        <div key={project.name} className={style.projectbox}>
+                    <div className={style.imagecontainor}>
                     <div className={style.imagebox}>
                     {/* <Image className={style.projectimg} width={1000} height={1000} src='' alt='Project'></Image> */}
                     </div>
                 </div>
                 <div className={style.projectdetails}>
-                    <h1 className={style.projectname}>Shri Umeshsons</h1>
-                    <p className={style.projecttext}>This project is make in Flutter. Client need to app and web app for his store he need to the app and web application</p>
+                    <h1 className={style.projectname}>{project.name}</h1>
+                    <p className={style.projecttext}>{project.discription}</p>
                 </div>
             </div>
-            <div className={style.projectbox}>
-                <div className={style.imagecontainor}>
-                    <div className={style.imagebox}>
-                    {/* <Image className={style.projectimg} width={1000} height={1000} src='' alt='Project'></Image> */}
-                    </div>
-                </div>
-                <div className={style.projectdetails}>
-                    <h1 className={style.projectname}>Shri Umeshsons</h1>
-                    <p className={style.projecttext}>This project is make in Flutter. Client need to app and web app for his store he need to the app and web application</p>
-                </div>
-            </div>
-            <div className={style.projectbox}>
-                <div className={style.imagecontainor}>
-                    <div className={style.imagebox}>
-                    {/* <Image className={style.projectimg} width={1000} height={1000} src='' alt='Project'></Image> */}
-                    </div>
-                </div>
-                <div className={style.projectdetails}>
-                    <h1 className={style.projectname}>Shri Umeshsons</h1>
-                    <p className={style.projecttext}>This project is make in Flutter. Client need to app and web app for his store he need to the app and web application</p>
-                </div>
-            </div>
-            <div className={style.projectbox}>
-                <div className={style.imagecontainor}>
-                    <div className={style.imagebox}>
-                    {/* <Image className={style.projectimg} width={1000} height={1000} src='' alt='Project'></Image> */}
-                    </div>
-                </div>
-                <div className={style.projectdetails}>
-                    <h1 className={style.projectname}>Shri Umeshsons</h1>
-                    <p className={style.projecttext}>This project is make in Flutter. Client need to app and web app for his store he need to the app and web application</p>
-                </div>
-            </div>
+                    )
+                })
+            }
         </div>
     )
 }
---
+++ src/lib/project.tsx
@@ -0,0 +1,19 @@
+const ProjectsDetails = [
+    {
+        image:'',
+        name: '17 Looms',
+        discription: 'The "17 Looms" project is centered around the creation of a dynamic and responsive website, coupled with tailored email templates to establish a compelling online presence for the brand.HTML, CSS, JavaScript: These foundational web technologies form the backbone of the project. HTML provides the structure of the website, defining its content and layout. CSS enhances the design by styling elements and ensuring visual coherence across different devices. JavaScript adds interactivity and functionality, enriching user interactions and optimizing performance.Responsive Website Design: Utilizing responsive web design principles ensures that the 17 Looms website adapts fluidly to various screen sizes and devices.Email Templates: Crafting bespoke email templates involves leveraging HTML for structure, CSS for styling, and JavaScript for dynamic content where applicable.Seamless integration of HTML, CSS, and JavaScript ensures smooth functionality and cross-browser compatibility.',
+    },
+    {
+        image: '',
+        name: 'Aaria',
+        discription: 'The "17 Looms" project is centered around the creation of a dynamic and responsive website, coupled with tailored email templates to establish a compelling online presence for the brand.HTML, CSS, JavaScript: These foundational web technologies form the backbone of the project. HTML provides the structure of the website, defining its content and layout. CSS enhances the design by styling elements and ensuring visual coherence across different devices. JavaScript adds interactivity and functionality, enriching user interactions and optimizing performance.Responsive Website Design: Utilizing responsive web design principles ensures that the 17 Looms website adapts fluidly to various screen sizes and devices.Email Templates: Crafting bespoke email templates involves leveraging HTML for structure, CSS for styling, and JavaScript for dynamic content where applicable.Seamless integration of HTML, CSS, and JavaScript ensures smooth functionality and cross-browser compatibility.',
+    },
+    {
+        image:'',
+        name: 'Shri Umeshsons',
+        discription: 'The "17 Looms" project is centered around the creation of a dynamic and responsive website, coupled with tailored email templates to establish a compelling online presence for the brand.HTML, CSS, JavaScript: These foundational web technologies form the backbone of the project. HTML provides the structure of the website, defining its content and layout. CSS enhances the design by styling elements and ensuring visual coherence across different devices. JavaScript adds interactivity and functionality, enriching user interactions and optimizing performance.Responsive Website Design: Utilizing responsive web design principles ensures that the 17 Looms website adapts fluidly to various screen sizes and devices.Email Templates: Crafting bespoke email templates involves leveraging HTML for structure, CSS for styling, and JavaScript for dynamic content where applicable.Seamless integration of HTML, CSS, and JavaScript ensures smooth functionality and cross-browser compatibility.',
+    }
+];
+
+export default ProjectsDetails;
