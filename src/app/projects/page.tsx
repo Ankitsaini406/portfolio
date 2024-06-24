19d3f5071e118fb450c8be5b41ecccb711d5d842
Ankit Saini <as.ankitsaini406@gmail.com>
2024-06-24 15:02:54 +0530

    "project page"

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,52 @@
+import style from './projects.module.css'
+
 const Projects = () => {
     return (
-        <div>
-            Projects
+        <div className={style.project}>
+            <div className={style.projectbox}>
+                <div className={style.imagecontainor}>
+                    <div className={style.imagebox}>
+                        
+                    </div>
+                </div>
+                <div className={style.projectdetails}>
+                    <h1 className={style.projectname}>Shri Umeshsons</h1>
+                    <p className={style.projecttext}>This project is make in Flutter. Client need to app and web app for his store he need to the app and web application</p>
+                </div>
+            </div>
+            <div className={style.projectbox}>
+                <div className={style.imagecontainor}>
+                    <div className={style.imagebox}>
+                        
+                    </div>
+                </div>
+                <div className={style.projectdetails}>
+                    <h1 className={style.projectname}>Shri Umeshsons</h1>
+                    <p className={style.projecttext}>This project is make in Flutter. Client need to app and web app for his store he need to the app and web application</p>
+                </div>
+            </div>
+            <div className={style.projectbox}>
+                <div className={style.imagecontainor}>
+                    <div className={style.imagebox}>
+                        
+                    </div>
+                </div>
+                <div className={style.projectdetails}>
+                    <h1 className={style.projectname}>Shri Umeshsons</h1>
+                    <p className={style.projecttext}>This project is make in Flutter. Client need to app and web app for his store he need to the app and web application</p>
+                </div>
+            </div>
+            <div className={style.projectbox}>
+                <div className={style.imagecontainor}>
+                    <div className={style.imagebox}>
+                        
+                    </div>
+                </div>
+                <div className={style.projectdetails}>
+                    <h1 className={style.projectname}>Shri Umeshsons</h1>
+                    <p className={style.projecttext}>This project is make in Flutter. Client need to app and web app for his store he need to the app and web application</p>
+                </div>
+            </div>
         </div>
     )
 }
