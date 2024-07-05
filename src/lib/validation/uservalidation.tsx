9ecfb5b0f26724c6ba2cf9e6d9df9cd8ee6c4347
Ankit Saini <as.ankitsaini406@gmail.com>
2024-07-05 10:15:02 +0530

    "uservalidation changes"

--- src/lib/validation/uservalidation.tsx
+++ src/lib/validation/uservalidation.tsx
@@ -5,4 +5,8 @@ export const userRegistrationSchema = z.object({
     email: z.string().email("Invalid email"),
     phonenumber: z.number().min(3, "Phone number is required").max(10, "Phone number max leanth is 10 charactres"),
     password: z.string().min(3, "Paswword must be at least 8 charactres"),
-})
+    re_password: z.string().min(3, "Paswword must be at least 8 charactres"),
+}).refine((data) => data.password === data.re_password, {
+    message: "Passwords don't match",
+    path: ["confirm"],
+});
