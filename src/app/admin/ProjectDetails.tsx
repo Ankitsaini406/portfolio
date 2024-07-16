9fc3ed69034811c6e23e123ee383f8c6d187408e
Ankit Saini <as.ankitsaini406@gmail.com>
2024-07-16 21:45:37 +0530

    "add project details"

--- src/app/admin/ProjectDetails.tsx
+++ src/app/admin/ProjectDetails.tsx
@@ -1,8 +1,17 @@
+import style from './admin.module.css';
 
 const ProjectDetails = () => {
     return (
-        <div>
-            <p>Project Details</p>
+        <div className={style.projectdetails}>
+            <div className={style.imgbox}>
+
+            </div>
+            <input className={style.inputbox} type="text" />
+            <textarea className={`${style.inputbox} ${style.desbox}`} />
+            <div className={style.btnflex}>
+            <button className={`${style.submitbutton} ${style.cancolor}`}>Cancal</button>
+            <button className={`${style.submitbutton} ${style.subcolor}`}>Submit</button>
+            </div>
         </div>
     )
 }
