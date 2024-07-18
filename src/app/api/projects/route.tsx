7325cd2893931acb7d539a3e13d2840ba00ca53b
Ankit Saini <as.ankitsaini406@gmail.com>
2024-07-18 18:09:13 +0530

    "changes in route"

--- src/app/api/projects/route.tsx
+++ src/app/api/projects/route.tsx
@@ -2,15 +2,13 @@ import ProjectModel from "@/lib/model/project";
 import connectToDatabase from "@/lib/mongoose/mongoose";
 import { NextResponse } from "next/server";
 
-
 export async function GET() {
     await connectToDatabase();
 
     try {
         const projects = await ProjectModel.find({});
-        // return projects;
         return NextResponse.json({status: 200, success: true, data: projects});
     } catch (error) {
-        return NextResponse.json({status: 500, success: false, error: `Failed to fetch timelines : ${error}`});
+        return NextResponse.json({status: 500, success: false, error: `Failed to fetch projects: ${error}`});
     }
 }
