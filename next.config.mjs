ee9a51ab5deab0acb0a21db39d631a42c84ee4e4
Ankit Saini <as.ankitsaini406@gmail.com>
2025-10-10 09:37:40 +0530

    change for the security

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,44 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+    {
+        key: "Strict-Transport-Security",
+        value: "max-age=31536000; includeSubDomains; preload",
+    },
+    {
+        key: "Content-Security-Policy",
+        value:
+            "default-src 'self'; img-src * data: blob:; media-src *; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; connect-src *; font-src 'self' data:; frame-ancestors 'self';",
+    },
+    {
+        key: "X-Frame-Options",
+        value: "SAMEORIGIN",
+    },
+    {
+        key: "X-Content-Type-Options",
+        value: "nosniff",
+    },
+    {
+        key: "Referrer-Policy",
+        value: "strict-origin-when-cross-origin",
+    },
+    {
+        key: "Permissions-Policy",
+        value: "geolocation=(), microphone=(), camera=()",
+    },
+];
+
 const nextConfig = {
-    images : {
-        domains: ["images.unsplash.com"],
-    }
+   poweredByHeader: fasle,
+
+    async headers() {
+       return [
+           {
+               source: "/(.*)",
+               headers: securityHeaders,
+           },
+       ];
+    },
 };
 
 export default nextConfig;
