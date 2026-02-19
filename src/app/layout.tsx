1f92b1423646d4ed4bf0477cd0ce6d2ad5f4464a
Ankit Saini <as.ankitsaini406@gmail.com>
2026-02-19 17:47:33 +0530

    update the script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,19 +91,23 @@ export default function RootLayout({
         </main>
         <Footer />
         {process.env.NODE_ENV === "production" && (
-      <>
-        <Script id="google-analytics" strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0PQLWEGNWL', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
-      </>
-    )}
+          <>
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-0PQLWEGNWL"
+              strategy="lazyOnload"
+            />
+            <Script id="google-analytics" strategy="lazyOnload">
+              {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-0PQLWEGNWL', {
+          page_path: window.location.pathname,
+        });
+      `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   );
