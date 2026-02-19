775926cb19634e11341ba119c242ed45c7ca6008
Ankit Saini <as.ankitsaini406@gmail.com>
2026-02-19 11:55:00 +0530

    update for the rating

--- next.config.mjs
+++ next.config.mjs
@@ -6,38 +6,28 @@ const securityHeaders = [
         value: "max-age=31536000; includeSubDomains; preload",
     },
     {
-        key: "Content-Security-Policy",
-        value:
-            "default-src 'self'; img-src * data: blob:; media-src *; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; connect-src *; font-src 'self' data:; frame-ancestors 'self';",
+        key: "X-Content-Type-Options",
+        value: "nosniff",
     },
     {
         key: "X-Frame-Options",
         value: "SAMEORIGIN",
     },
     {
-        key: "X-Content-Type-Options",
-        value: "nosniff",
-    },
-    {
         key: "Referrer-Policy",
         value: "strict-origin-when-cross-origin",
-    },
-    {
-        key: "Permissions-Policy",
-        value: "geolocation=(), microphone=(), camera=()",
     },
 ];
 
 const nextConfig = {
-   poweredByHeader: false,
-
+    poweredByHeader: false,
     async headers() {
-       return [
-           {
-               source: "/(.*)",
-               headers: securityHeaders,
-           },
-       ];
+        return [
+            {
+                source: "/(.*)",
+                headers: securityHeaders,
+            },
+        ];
     },
 };
 
